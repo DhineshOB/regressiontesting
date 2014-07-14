e88a7401d5915d004ef561c37562e501896246f6
dhineshob <obdhinesh@ymail.com>
2014-07-14 14:12:08 -0700

    changes in method 2

--- testjacoco/src/test/java/sampleTest.java
+++ testjacoco/src/test/java/sampleTest.java
@@ -11,7 +11,9 @@ public class sampleTest {
 	  System.out.println("Hello Testng");
 	  sample test = new sample();
 	  String result = test.method1();
+	  String result2= test.method2();
 	  Assert.assertEquals(result, "Method 1");
+	  Assert.assertEquals(result, "Method 2");
 	  
   }
 }
