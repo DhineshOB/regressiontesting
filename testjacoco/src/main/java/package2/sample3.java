8949aa6a5cd04f7818c3c5133a2cc0e52890e904
dhineshob <obdhinesh@ymail.com>
2014-07-14 14:18:53 -0700

    added sample 2 and sample 3

---
+++ testjacoco/src/main/java/package2/sample3.java
@@ -0,0 +1,16 @@
+package package2;
+
+
+public class sample3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int a=10;
+			while(a==20)
+			System.out.println("twenty");
+		
+		
+
+	}
+
+}
