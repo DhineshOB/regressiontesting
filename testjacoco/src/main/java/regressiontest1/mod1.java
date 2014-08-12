424619459587df4af1487d4f84cdb288f1c9d836
dhineshob <obdhinesh@ymail.com>
2014-08-12 12:20:55 -0700

    new commit

---
+++ testjacoco/src/main/java/regressiontest1/mod1.java
@@ -0,0 +1,15 @@
+package regressiontest1;
+
+public class mod1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int a=10;
+			while(a==10)
+			System.out.println("Ten");
+		
+
+	}
+
+}
