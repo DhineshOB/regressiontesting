3130eec59b41bc226863af8fa8ec3a029912a634
dhineshob <obdhinesh@ymail.com>
2014-08-13 15:15:27 -0700

    changes to mod3

--- testjacoco/src/test/java/regression3/Mod3Test.java
+++ testjacoco/src/test/java/regression3/Mod3Test.java
@@ -5,11 +5,17 @@ import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import regression2.Mod2;
+
 public class Mod3Test {
   @Test
   public void selection() {
 	  System.out.println("Testng Running Mod3Test.......");
-	  
+	  Mod3 test = new Mod3();
+	  String result = test.method1();
+	  String result2= test.method2();
+	  Assert.assertEquals(result, "Method 1");
+	  Assert.assertEquals(result2, "Method 2");
 	  
   }
 }
