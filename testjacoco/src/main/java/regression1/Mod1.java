53d08bb87b4bbef1b943c5ce86bd60b30d8691e6
dhineshob <obdhinesh@ymail.com>
2014-08-13 14:36:57 -0700

    changes in mod1test

--- testjacoco/src/main/java/regression1/Mod1.java
+++ testjacoco/src/main/java/regression1/Mod1.java
@@ -1,14 +1,47 @@
 package regression1;
 
 
+
+
 public class Mod1 {
 
+		public String method1()
+		{
+			
+			String result1 ="Method 1";
+			return result1;
+		
+		}
+		
+		public String method2()
+		{
+			
+			String result2 ="Method 2";
+			return result2;
+		
+		}
+		
+		public String method3()
+		{
+			
+			String result3 ="Method 3";
+			return result3;
+		
+		
+		}
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			int a=10;
-			while(a==10)
-			System.out.println("Ten");
+		int a=10;
+		Mod1 sam = new Mod1();
+		if(a==10)
+		sam.method1();
+		else if(a==20)
+		sam.method2();
+		else
+		sam.method3();
+		System.out.println("End of Exe");
 		
 
 	}
--- testjacoco/src/test/java/regression1/Mod1Test.java
+++ testjacoco/src/test/java/regression1/Mod1Test.java
@@ -6,10 +6,16 @@ import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import regression2.Mod2;
+
 public class Mod1Test {
   @Test
   public void selection() {
 	  System.out.println("Testng Running Mod1Test.......");
-	 
+	  Mod1 test = new Mod1();
+	  String result = test.method1();
+	  String result2= test.method2();
+	  Assert.assertEquals(result, "Method 1");
+	  Assert.assertEquals(result2, "Method 2");
   }
 }
