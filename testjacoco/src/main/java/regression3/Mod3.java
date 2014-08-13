5a41b9d4fd31639c7f36e3acdca3b960f0955b77
dhineshob <obdhinesh@ymail.com>
2014-08-13 15:08:08 -0700

    changes to mod3

--- testjacoco/src/main/java/regression3/Mod3.java
+++ testjacoco/src/main/java/regression3/Mod3.java
@@ -4,41 +4,11 @@ package regression3;
 
 public class Mod3 {
 
-	public String method1()
-	{
-		
-		String result1 ="Method 1";
-		return result1;
 	
-	}
-	
-	public String method2()
-	{
-		
-		String result2 ="Method 2";
-		return result2;
-	
-	}
-	
-	public String method3()
-	{
-		
-		String result3 ="Method 3";
-		return result3;
-	
-	
-	}
 	
 	public static void main(String args[])
 	{
-		int a=10;
-		Mod3 sam = new Mod3();
-		if(a==10)
-		sam.method1();
-		else if(a==20)
-		sam.method2();
-		else
-		sam.method3();
+		
 		System.out.println("End of Exe");
 	}
 	
